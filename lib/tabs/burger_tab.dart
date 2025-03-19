c457553dc3bc646bc778e018b8fd5a01ded51847
iJvpiter <rocketfx896@gmail.com>
2025-03-19 10:21:49 -0600

    Adding new products

--- lib/tabs/burger_tab.dart
+++ lib/tabs/burger_tab.dart
@@ -1,12 +1,88 @@
+import 'package:donut_app_2a_martinez/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class BurgerTab extends StatelessWidget {
-  const BurgerTab({super.key});
+  // Lista de donas
+  final List donutsOnSale = [
+    // [ donutFlavor, donutStore, donutPrice, donutColor, imageName ]
+    [
+      "Ice Cream",
+      "Krispy Kreme",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Kreme",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+    [
+      "Ice Cream",
+      "Krispy Kreme",
+      "36",
+      Colors.blue,
+      "lib/images/icecream_donut.png"
+    ],
+    [
+      "Strawberry",
+      "Dunkin donuts",
+      "45",
+      Colors.red,
+      "lib/images/strawberry_donut.png"
+    ],
+    [
+      "Grape Ape",
+      "Krispy Kreme",
+      "84",
+      Colors.purple,
+      "lib/images/grape_donut.png"
+    ],
+    [
+      "Choco",
+      "Dunkin donuts",
+      "95",
+      Colors.brown,
+      "lib/images/chocolate_donut.png"
+    ],
+  ];
+
+  BurgerTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("BurgerTab"),
+    return GridView.builder(
+      itemCount: donutsOnSale.length,
+      padding: const EdgeInsets.all(12),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.5,
+      ),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutStore: donutsOnSale[index][1],
+          donutPrice: donutsOnSale[index][2],
+          donutColor: donutsOnSale[index][3],
+          imageName: donutsOnSale[index][4],
+        );
+      },
     );
   }
 }
